6b02dd5855231c037a6b19db9f6358dfbb58f7d1
reyes lopez <laloeduar822@gmail.com>
2025-07-04 10:30:32 -0600

    sensor taza

--- Cafetera/Sensor.cs
+++ Cafetera/Sensor.cs
@@ -5,13 +5,24 @@
         public bool detectaAgua()
         {
             Console.WriteLine("Sensor: Hay suficiente agua");
+            if (DateTime.Now.Second % 2 == 0)
+            {
+                Console.WriteLine("Sensor: Agua insuficiente");
+                return false; // Simula que no hay suficiente agua
+            }
             return true;
         }
 
         public bool detectarTaza()
         {
             Console.WriteLine("Sensor: Taza colocada");
+            if (DateTime.Now.Second % 2 == 0)
+            {
+                Console.WriteLine("Sensor: Taza no detectada");
+                return false; // Simula que no hay taza colocada
+            }
             return true;
+
         }
     }
 }
