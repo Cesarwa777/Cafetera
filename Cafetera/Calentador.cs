fc28d773423f7bdebb3e11289622d99d4a0cc97b
katia valencia <vkatia079@gmail.com>
2025-07-04 10:45:24 -0600

    master katia

--- Cafetera/Calentador.cs
+++ Cafetera/Calentador.cs
@@ -12,8 +12,4 @@
             public void apagar()
             {
                 Console.WriteLine("Apagando cafetera \n ¡CUIDADO SIGUE CALIENTE!");
-                Console.WriteLine("Cafetera apagada");
-            }
-        }
-    }
 }
