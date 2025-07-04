be973139d333dd949837c73def1a41454f6243f4
reyes lopez <laloeduar822@gmail.com>
2025-07-04 10:35:35 -0600

    ifffff

--- Cafetera/Sensor.cs
+++ Cafetera/Sensor.cs
@@ -7,7 +7,7 @@
             Console.WriteLine("Sensor: Hay suficiente agua");
             if (DateTime.Now.Second % 2 == 0)
             {
-                Console.WriteLine("Sensor: Agua insuficiente");
+                Console.WriteLine("Sensor: Agua insuficiente:");
                 return false; // Simula que no hay suficiente agua
             }
             return true;
